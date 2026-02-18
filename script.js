48d5bc1860bad94a7683f29779f0037e4bc1d77c
LisaRupert03 <lisarupert03@gmail.com>
2026-02-18 10:59:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const faders = document.querySelectorAll(".fade-in");
+
+window.addEventListener("scroll", () => {
+    faders.forEach(fader => {
+        const rect = fader.getBoundingClientRect();
+        if(rect.top < window.innerHeight - 100){
+            fader.classList.add("visible");
+        }
+    });
+});
